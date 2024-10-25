bb7e850d858da9c259ac37e06af72c0e745d2682
florentinadtrn <162402831+florentinadtrn@users.noreply.github.com>
2024-10-26 06:13:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Responsive Layout Example',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: ResponsiveLayout(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class ResponsiveLayout extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: Row(
+     
+      body: Column(
         children: [
-          // Sidebar
-          Expanded(
-            flex: 1,
-            child: Container(
-              color: Colors.blueGrey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Dasboard',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.home, color: Colors.white), // Ikon untuk Menu 1
-                    title: Text(
-                      'Home',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.settings, color: Colors.white), // Ikon untuk Menu 2
-                    title: Text(
-                      'Settings',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.info, color: Colors.white), // Ikon untuk Menu 3
-                    title: Text(
-                      'About',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.person, color: Colors.white), // Ikon untuk Menu 4
-                    title: Text(
-                      'Akun',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
+          Text("Pembimbing Akademik"),
+          CircleAvatar(
+            backgroundImage: NetworkImage("https://awsimages.detik.net.id/community/media/visual/2020/07/04/ji-chang-wook-5_169.jpeg?w=600&q=90")
+
           ),
-          // Main Content Area
-          Expanded(
-            flex: 4,
-            child: Column(
-              children: [
-                // Header
-                Container(
-                  height: 60,
-                  color: Colors.blue,
-                  child: Center(
-                    child: Text(
-                      'Travelease',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                // Content
-                Expanded(
-                  child: Container(
-                    color: Colors.white,
-                    padding: EdgeInsets.all(16.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Text(
-                            'Atur jadwal dan nikmati perjalanan wisata anda dengan layanan inklusif dari Travelease',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          Row(
+            children:  [
+              Icon(Icons.person),
+              Text("Kadek Darmaastawan")
+            ]
+          ),
+          Row(
+            children: [
+              Icon(Icons.email),
+              Text("darmaastawan@undiknas.ac.id")
+            ]
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
